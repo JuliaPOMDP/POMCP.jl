f090f4ffdfbeb2e973ad09ccd57ba6648119b702
Zachary Sunberg <sunbergzach@gmail.com>
2016-05-26 08:10:41 -0700

    added visualization tests

---
+++ test/visualization.jl
@@ -0,0 +1,14 @@
+solver = POMCPSolver(rollout_solver=FeedWhenCrying(),
+                    eps=0.01,
+                    c=10.0,
+                    tree_queries=50, 
+                    rng=MersenneTwister(2))
+
+problem = BabyPOMDP()
+policy = solve(solver, problem)
+up = updater(policy)
+belief = initialize_belief(up, initial_state_distribution(problem))
+action(policy, belief)
+
+dummy = IOBuffer()
+writemime(dummy, MIME("text/html"), POMCPTreeVisualizer(belief))
