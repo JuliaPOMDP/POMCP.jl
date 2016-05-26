de63f41cdb65a800f88f7ecba7501bdb870881d6
Zachary Sunberg <sunbergzach@gmail.com>
2016-05-26 07:40:31 -0700

    visualization works

--- src/visualization.jl
+++ src/visualization.jl
@@ -24,8 +24,26 @@ function recursive_push!(nd::NodeDict, n::BeliefNode, parent_id=-1)
     nd[id] = Dict("id"=>id,
                   "type"=>:belief, # maybe this should be :state - it doesn't matter for now
                   "children_ids"=>Array(Int,0),
-                  "tag"=>node_tag(n),
-                  "tt_tag"=>tooltip_tag(n),
+                  "tag"=>node_tag(n.label),
+                  "tt_tag"=>tooltip_tag(n.label),
+                  "N"=>n.N
+                  )
+    for (a,c) in n.children
+        recursive_push!(nd, c, id)
+    end
+    return nd
+end
+
+function recursive_push!(nd::NodeDict, n::RootNode, parent_id=-1)
+    id = length(nd) + 1
+    if parent_id > 0
+        push!(nd[parent_id]["children_ids"], id)
+    end
+    nd[id] = Dict("id"=>id,
+                  "type"=>:belief, # maybe this should be :state - it doesn't matter for now
+                  "children_ids"=>Array(Int,0),
+                  "tag"=>node_tag(n.B),
+                  "tt_tag"=>tooltip_tag(n.B),
                   "N"=>n.N
                   )
     for (a,c) in n.children
@@ -42,9 +60,10 @@ function recursive_push!(nd::NodeDict, n::ActNode, parent_id=-1)
     nd[id] = Dict("id"=>id,
                   "type"=>:action,
                   "children_ids"=>Array(Int,0),
-                  "tag"=>node_tag(n),
-                  "tt_tag"=>tooltip_tag(n),
-                  "N"=>n.N
+                  "tag"=>node_tag(n.label),
+                  "tt_tag"=>tooltip_tag(n.label),
+                  "N"=>n.N,
+                  "Q"=>n.V
                   )
     for (o,c) in n.children
         recursive_push!(nd, c, id)
@@ -52,15 +71,12 @@ function recursive_push!(nd::NodeDict, n::ActNode, parent_id=-1)
     return nd
 end
 
-# TODO: set to download javascript and css from MCTS repository
-
 function writemime(f::IO, ::MIME"text/html", visualizer::POMCPTreeVisualizer)
-    #=
     json, root_id = create_json(visualizer)
     # write("/tmp/tree_dump.json", json)
-    css = readall(joinpath(dirname(@__FILE__()), "tree_vis.css"))
-    js = readall(joinpath(dirname(@__FILE__()), "tree_vis.js"))
-    div = "trevis$(randstring())"
+    css = readall(joinpath(Pkg.dir("MCTS"), "src", "tree_vis.css"))
+    js = readall(joinpath(Pkg.dir("MCTS"), "src", "tree_vis.js"))
+    div = "treevis$(randstring())"
 
     html_string = """
         <div id="$div">
@@ -76,8 +92,7 @@ function writemime(f::IO, ::MIME"text/html", visualizer::POMCPTreeVisualizer)
         </script>
         </div>
     """
-    =#
-    html_string = "visualization doesn't work yet :("
+    # html_string = "visualization doesn't work yet :("
 
     # for debugging
     # outfile  = open("/tmp/pomcp_debug.html","w")
