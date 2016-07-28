0cc03aeb88b5153fd61288202cfda6fc10c4ce5e
Zachary Sunberg <sunbergzach@gmail.com>
2016-07-28 13:48:38 -0700

    moved sample to POMDPToolbox

--- src/POMCP.jl
+++ src/POMCP.jl
@@ -87,24 +87,6 @@ type POMCPPlanner{S, A, O, SolverType} <: POMDPs.Policy
     _tree_ref::Nullable{Any}
 end
 
-#### MISC CONVENIENCE FUNCTIONS ###
-# XXX kinda sloppy, not sure what a better place is
-
-function sample(rng::AbstractRNG, wv::WeightVec)
-    t = rand(rng) * sum(wv)
-    w = values(wv)
-    n = length(w)
-    i = 1
-    cw = w[1]
-    while cw < t && i < n
-        i += 1
-        @inbounds cw += w[i]
-    end
-    return i
-end
-
-sample(rng::AbstractRNG, a::AbstractArray, wv::WeightVec) = a[sample(rng,wv)]
-
 include("tree.jl")
 include("constructor.jl")
 include("particle_filter.jl")
