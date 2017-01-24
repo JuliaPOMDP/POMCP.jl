e230edd2bb2d427dd3f495fd3dd15486346f764b
Zachary Sunberg <sunbergzach@gmail.com>
2017-01-23 23:09:23 -0800

    fixed syntax in make

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, POMCP
 makedocs(modules=[POMCP])
 
 deploydocs(
-    deps = Deps.pip("mkdocs", "python-markdown-math")
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/JuliaPOMDP/POMCP.jl.git",
     julia = "0.5",
     osname = "linux"
